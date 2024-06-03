97f24074a66a9afd5f2835779b6624a565fc8f18
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-06-03 18:24:30 -0400

    Lista la clase Articulos

---
+++ Models/Articulos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ap1_P1_JoseRivera.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public decimal? Costo { get; set; }
+
+        public decimal? Ganancia { get; set; }
+
+        public decimal? Precio { get; set; }
+    }
+}
+
